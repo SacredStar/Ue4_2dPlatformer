18cc995e77b44819ad9f59a7161a699fdc557bcf
shmelevka <shadowordeath1993@yandex.ru>
2019-05-16 22:17:59 +0300

    Initial Commit

---
+++ EscapeTheRoom/Source/EscapeTheRoom/EscapeTheRoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEROOM_API AEscapeTheRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
